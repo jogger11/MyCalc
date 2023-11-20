6c0fbdf72b1d7f1e467c24c9ffe526690171a946
Anton Polyakov <p-anton-v@yandex.ru>
2023-11-20 23:21:50 +0500

    Initial_Commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine().trim(); // sc.nextLine(); // ввод сроки выражения
@@ -16,12 +16,12 @@ public class Main {
         System.out.println(calc(str));
     }
 
-    public static String calc(String input) {  //метод, который необходимо реализовать
+    public static String calc(String input) throws Exception {  //метод, который необходимо реализовать
         Operations operations = new Operations(); // создаем объект класса Operations
 
         int index = operations.index_znak(input); // возвращаем индекс знака операции в заданном выражении, а также проверяем количество знаков опреаций (если больше 1, то возвращаем -1)
         if (index == -1) { //выброс исключения по количетсву знаков опереаций в заданном выражении
-            return "throws Exception";
+            throw new Exception();
         }
 
         String type_znak = String.valueOf(input.charAt(index)); // выделение знака операции в выражении
@@ -29,30 +29,30 @@ public class Main {
         String a2 = input.substring(index + 1).trim(); // выделение 2-го числа, которое слева от знака
 
         if (a2.equals("0") && type_znak.equals("/")) { //выброс исключения "деление на 0"
-            return "throws Exception";
+            throw new Exception();
         }
 
         String type_a1 = operations.digit(a1); // определяем тип цифр в числе a1 и корректность ввода числа
         String type_a2 = operations.digit(a2); // определяем тип цифр в числе a2 и корректность ввода числа
 
         if (!(type_a1.equals(type_a2)) || type_a1.equals("false") || type_a2.equals("false")) { //выброс исключения по не соответствию типа цифр и корректности ввода числа
-            return "throws Exception";
+            throw new Exception();
         }
 
         int rezult = 0;
         int x1 = 0;
         int x2 = 0;
 
-        if (type_a1.equals(type_a2) && type_a1.equals("arabia")) {
+        if (type_a1.equals("arabia")) {
             x1 = Integer.parseInt(a1); // перевод из строкового типа данных в целые
             x2 = Integer.parseInt(a2); // перевод из строкового типа данных в целые
         }
-        if (type_a1.equals(type_a2) && type_a1.equals("roma")) {
+        if (type_a1.equals("roma")) {
             x1 = operations.to_arabia(a1); // перевод из римских цифр в арабские
             x2 = operations.to_arabia(a2); // перевод из римских цифр в арабские
         }
         if (x1 == -1 || x2 == -1) { //выброс исключения: число не является римским
-            return "throws Exception";
+            throw new Exception();
         }
 
         switch (type_znak) { // расчет значения в арабских числах
@@ -72,15 +72,15 @@ public class Main {
         String s1;
         if (type_a1.equals("arabia")) {  //вывод результатов, если были арабские числа
             s1 = String.valueOf(rezult);
-            return (a1 + " " + type_znak + " " + a2 + " = " + s1);
+            return s1;
         }
 
         if (type_a1.equals("roma")) { //вывод результатов, если были римские числа
             if (rezult < 1) {
-                return "throws Exception"; //выброс исключения: ответ меньше 1 для римских чисел
+                throw new Exception(); //выброс исключения: ответ меньше 1 для римских чисел
             } else {
                 String rezult_roma = operations.to_roma(rezult);
-                return (a1 + " " + type_znak + " " + a2 + " = " + rezult_roma);
+                return rezult_roma;
             }
 
         }
